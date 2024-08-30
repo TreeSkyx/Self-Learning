6dbb836c1abf5745313743dd0b630627467bd9be
TreeSkyx <wiroon.som@gmail.com>
2024-08-30 13:29:50 +0700

    intitial commit for vitest

--- redux-simple/src/app/page.tsx
+++ redux-simple/src/app/page.tsx
@@ -1,27 +1,46 @@
-'use client';
+"use client";
 import type { RootState } from "./redux/store";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, incrementByAmount, decrementByAmount } from "./redux/features/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import phase from "react-html-parser";
 
 export default function Home() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
   return (
-    <main className = "flex flex-col items-center justify-between p-24 gap-5">
-      <div className = "font-bold text-2xl">Count by Redux Toolkit</div> 
-      <div className= "font-bold text-xl">{count}</div>
-      <button className="bg-blue-500 rounded-full p-2" onClick={() => dispatch(increment())}>
+    <main className="flex flex-col items-center justify-between p-24 gap-5">
+      <div className="font-bold text-2xl">Count by Redux Toolkit</div>
+      <div className="font-bold text-xl">{count}</div>
+      <button
+        className="bg-blue-500 rounded-full p-2"
+        onClick={() => dispatch(increment())}
+      >
         Increment
-        </button> 
-        <button className="bg-red-500 rounded-full p-2" onClick={() => dispatch(decrement())}>
+      </button>
+      <button
+        className="bg-red-500 rounded-full p-2"
+        onClick={() => dispatch(decrement())}
+      >
         Decrement
-        </button> 
-        <button className=" bg-teal-500 rounded-full p-2" onClick={() => dispatch(incrementByAmount(2))}>
+      </button>
+      <button
+        className=" bg-teal-500 rounded-full p-2"
+        onClick={() => dispatch(incrementByAmount(2))}
+      >
         Increment by 2
-        </button>
-        <button className=" bg-cyan-500 rounded-full p-2" onClick={() => dispatch(decrementByAmount(2))}>
+      </button>
+      <button
+        className=" bg-cyan-500 rounded-full p-2"
+        onClick={() => dispatch(decrementByAmount(2))}
+      >
         Decrement by 2
-        </button> 
+      </button>
+      HTML Phaser
     </main>
   );
 }
---
+++ vitest-app/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Counter } from "./counter.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
